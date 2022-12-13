d22cc5cf2cd64f6aa82cf4a34d85398f666f75e9
Kzvuong <118218278+Kzvuong@users.noreply.github.com>
2022-12-13 12:45:05 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,31 @@
 let Bad_Guy: Sprite = null
+let statusbar: StatusBarSprite = null
 scene.setBackgroundColor(1)
 let Person = sprites.create(img`
-    . f f f . 
-    . f f f . 
-    . f f f . 
-    . . f . . 
-    . f f f . 
-    f . f . f 
-    . . f . . 
-    . . f . . 
-    . f . f . 
-    f . . . f 
+    . f f f . . . . . . 
+    . f f f . . . . . . 
+    . f f f . . . . . . 
+    . . f . . . . . . . 
+    . f f f . . . . . . 
+    f . f . f . . . . . 
+    . . f . . . . . . . 
+    . . f . . . . . . . 
+    . f . f . . . . . . 
+    f . . . f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(Person)
 game.onUpdateInterval(500, function () {
+    statusbar = statusbars.create(20, 4, StatusBarKind.Health)
     Bad_Guy = sprites.create(img`
+        . 7 2 2 . 
         . 2 2 2 . 
-        . 2 2 2 . 
-        . 2 2 2 . 
+        . 2 2 7 . 
         . . 2 . . 
-        . 2 2 2 . 
-        2 . 2 . 2 
-        . . 2 . . 
+        . 7 2 2 . 
+        2 . 2 . 7 
+        . . 7 . . 
         . . 2 . . 
         . 2 . 2 . 
-        2 . . . 2 
+        7 . . . 2 
         `, SpriteKind.Player)
 })
